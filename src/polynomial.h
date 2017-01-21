4840445545550728a8a10429c11cd4d85e672cce
Dillon Huff <dillonhuff@gmail.com>
2017-01-20 19:02:42 -0800

    Now have real test of the remainder

--- src/polynomial.h
+++ src/polynomial.h
@@ -6,27 +6,88 @@ namespace easy_polly {
 
   template<typename N>
   class monomial {
+    N c;
+    std::vector<int> powers;
+    int nvars;
 
   public:
     monomial(const N& p_coeff,
 	     const std::vector<int> p_powers,
-	     const int p_num_vars) {}
+	     const int p_num_vars) :
+      c(p_coeff),
+      powers(p_powers),
+      nvars(p_num_vars) {}
+
+    N coeff() const { return c; }
+    int num_vars() const { return nvars; }
+    int power(const int i) const { return powers[i]; }
+
+    template<typename Comparator>
+    bool equals(Comparator eq, const monomial<N>& g) const {
+      assert(g.num_vars() == num_vars());
+
+      if (!eq(g.coeff(), coeff())) { return false; }
+
+      for (int i = 0; i < g.num_vars(); i++) {
+	if (g.power(i) != power(i)) {
+	  return false;
+	}
+      }
+
+      return true;
+    }
+    
 
   };
 
   template<typename N>
   bool lexicographic_order(const monomial<N>& x,
 			   const monomial<N>& y) {
-    assert(false);
+    assert(x.num_vars() == y.num_vars());
+
+    for (int i = 0; i < x.num_vars(); i++) {
+      if (x.power(i) < y.power(i)) {
+	return true;
+      } if (x.power(i) > y.power(i)) {
+	return false;
+      }
+    }
+
+    // x == y
+    return false;
   }
 
 
   template<typename N>
   class polynomial {
 
+    std::vector<monomial<N> > monos;
+    int num_vars;
+
   public:
     polynomial(const std::vector<monomial<N> > p_monos,
-	       const int p_num_vars) {}
+	       const int p_num_vars) {
+      monos = p_monos;
+      std::sort(begin(monos), end(monos), lexicographic_order<N>);
+      num_vars = p_num_vars;
+    }
+
+    int num_monos() const { return monos.size(); }
+
+    const monomial<N>& monomial(const int i) const { return monos[i]; }
+
+    template<typename Comparator>
+    bool equals(Comparator eq, const polynomial<N>& g) const {
+      if (g.num_monos() != num_monos()) { return false; }
+
+      for (unsigned i = 0; i < g.num_monos(); i++) {
+	if ( !g.monomial(i).equals(eq, monomial(i)) ) {
+	  return false;
+	}
+      }
+
+      return true;
+    }
 
   };
 
@@ -47,7 +108,7 @@ namespace easy_polly {
   bool poly_eq(Comparator eq,
 	       const polynomial<N>& f,
 	       const polynomial<N>& g) {
-    assert(false);
+    return f.equals(eq, g);
   }
   
 }
--- test/division_tests.cpp
+++ test/division_tests.cpp
@@ -20,19 +20,28 @@ namespace easy_polly {
     polynomial<double> g1{{{1.0, {1, 1}, 2}, {1.0, {1, 0}, 2}}, 2};
     polynomial<double> g2{{{1.0, {2, 0}, 2}, {1.0, {0, 0}, 2}}, 2};
 
-    std::vector<polynomial<double> > g{g1, g2};
-
     polynomial<double> a1{{{1.0, {1, 0}, 2}}, 2};
     polynomial<double> a2{{{-1.0, {0, 0}, 2}}, 2};
     polynomial<double> r{{{-1.0, {0, 1}, 2}, {-1.0, {0, 0}, 2}}, 2};
 
     std::vector<polynomial<double> > as{a1, a2};
-    
-    division_result<double> expected_result{as, r};
+    division_result<double> expected_result{as, r};    
 
-    division_result<double> res = divide(f, g, lexicographic_order<double>);
+    SECTION("Remainder is correct") {
 
-    REQUIRE(poly_eq(double_eq, res.remainder, expected_result.remainder));
+      std::vector<polynomial<double> > g{g1, g2};
+      division_result<double> res = divide(f, g, lexicographic_order<double>);
+
+      REQUIRE(poly_eq(double_eq, res.remainder, expected_result.remainder));
+    }
+
+    SECTION("Remainder changes for ") {
+      std::vector<polynomial<double> > g{g2, g1};
+      division_result<double> res = divide(f, g, lexicographic_order<double>);
+
+      REQUIRE(!poly_eq(double_eq, res.remainder, expected_result.remainder));
+    }
+
   }
   
 }
